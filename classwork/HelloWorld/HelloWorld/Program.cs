d7cb2b825d1747ba2d19c4653bffb6b080fcfa13
DJ July <digge@LAPTOP-JVU329AL>
2020-08-26 21:35:21 -0500

    C# Fundamentals

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -7,23 +7,41 @@ using System;
 
 namespace HelloWorld
 {
+    // Pascal Casing - Capitalize on word boundaries including first word
+    // Camel Casing - Capitalize on word boundaries except first word
     class Program
     {
-        static void Main(string[] args)
+        // Fucntion Declaration - declaration to compiler that a faunction exists with a given signiture
+        // Function Signiture - function name and the parameter types ( somtimes will include retunr types)
+        // Functoin defintion - declaration + implementation
+        static void Main(string[] args) // 1 parameter
         {
             // A single line comment
             // Another line of comment
-            Console.WriteLine("Hello World!");
+            
+            // Display given string to output
+            Console.WriteLine("Hello World!"); // printf, cout
 
-            int hours = 40;
-            double payRate = 12.80;
+            // Variable Declaration
+            // T- type, ID-identifier, T id;
+            //int hours;
+            //hours = 10; // Assignment operator: id = E
 
-            double totalPay = hours * payRate;
+            // Identifier Rules
+            // 1. Unique withine scope
+            // 2. Must start with underscore or letter
+            // 3. Consist of letter, digits, and underscores
+            // Always Camel Case local variables, noun
+            // Preferred: T id = E;
+            int hours = 10;
 
-            // Another block
-            // 10 + 45;
-            // NO!!! { Console.WriteLine(); }
+            // int pay = 0;
+            //pay = hours * 9;
+            int totalPay = hours * 9;
 
+            // Function overload - multiple functions with same name but different parameters
+            // atof, atoi
+            Console.WriteLine(totalPay);
         }
     }
 }
